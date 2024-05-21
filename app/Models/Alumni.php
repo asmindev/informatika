a140c44c800c5091ca5569115e7a3dacf61e4e5a
asmindev <asmin987asmin@gmail.com>
2024-05-21 10:02:59 +0800

    feat: init

--- app/Models/Alumni.php
+++ app/Models/Alumni.php
@@ -9,4 +9,13 @@ class Alumni extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'image', 'nim', 'graduation_year'];
+    public function getImageAttribute($value)
+    {
+        // if value starts with http
+        if (str_starts_with($value, 'http')) {
+            return $value;
+        } else {
+            return asset("storage/$value");
+        }
+    }
 }
