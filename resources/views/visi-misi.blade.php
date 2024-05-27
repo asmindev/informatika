7d4f9586da07eddc9c97d42faa816764d93dff82
iqkyu5566 <ilham5566@gmail.com>
2024-05-27 19:04:29 +0800

    feat : perubahan visi - misi

--- resources/views/visi-misi.blade.php
+++ resources/views/visi-misi.blade.php
@@ -2,34 +2,42 @@
     <section class="w-full h-full px-4 md:w-11/12 lg:w-10/12 mx-auto">
         <div class="w-full my-12 py-8 px-12 rounded-xl bg-slate-100">
             <h1 class="text-3xl font-bold text-slate-800 uppercase">Visi & Misi</h1>
-            <p class="text-slate-700 mt-4">Berikut adalah visi dan misi Jurusan Teknik Informatika Universitas Halu Oleo</p>
+            <p class="text-slate-700 mt-4">Berikut adalah visi dan misi Jurusan Teknik Informatika Universitas Halu Oleo
+            </p>
 
         </div>
         <div class="my-24">
             <h1 class="font-semibold">
-                Visi & Misi Jurusan Teknik Informatika UHO
+                Visi & Misi Jurusan Teknik Informatika UHO 2023
             </h1>
             <article>
                 <h5 class="mt-4 text-slate-800 font-semibold">Visi</h5>
                 <p class="mt-4 text-slate-700">
-                    Pada tahun 2019 menjadi Program Studi Teknik Informatika terdepan di tingkat Indonesia Timur, yang menghasilkan sumber daya manusia profesional dan berdaya saing tinggi dalam pengembangan Teknologi Informasi dan Komunikasi di wilayah pedesaan dan pesisir.
+                    Pada tahun 2019 menjadi Program Studi Teknik Informatika terdepan di tingkat Indonesia Timur, yang
+                    menghasilkan sumber daya manusia profesional dan berdaya saing tinggi dalam pengembangan Teknologi
+                    Informasi dan Komunikasi di wilayah pedesaan dan pesisir.
                 </p>
                 <h5 class="mt-4 text-slate-800 font-semibold">Misi</h5>
                 <ul class="mt-4 text-slate-700">
                     <li>
-                        Menyelenggarakan pendidikan dan pengajaran berbasis riset bidang informatika dengan memanfaatkan kemajuan teknologi informasi dan komunikasi.
+                        Menyelenggarakan pendidikan dan pengajaran berbasis riset bidang informatika dengan memanfaatkan
+                        kemajuan teknologi informasi dan komunikasi.
                     </li>
                     <li>
-                        Mengembangkan penelitian unggulan yang berorientasi pada publikasi dan perolehan hak atas kekayaan intelektual.
+                        Mengembangkan penelitian unggulan yang berorientasi pada publikasi dan perolehan hak atas
+                        kekayaan intelektual.
                     </li>
                     <li>
-                        Menerapkan hasil-hasil penelitian dan produk unggulan lainnya bagi kesejahteraan institusi, masyarakat dan kemajuan IPTEKS.
+                        Menerapkan hasil-hasil penelitian dan produk unggulan lainnya bagi kesejahteraan institusi,
+                        masyarakat dan kemajuan IPTEKS.
                     </li>
                     <li>
-                        Menyelenggarakan sistem tata kelola program studi yang transparan dan akuntabel sehingga mampu memberikan pelayanan prima dalam pendidikan.
+                        Menyelenggarakan sistem tata kelola program studi yang transparan dan akuntabel sehingga mampu
+                        memberikan pelayanan prima dalam pendidikan.
                     </li>
                     <li>
-                        Mengembangkan potensi mahasiswa di bidang Teknologi Informasi dan Komunikasi wilayah pedesaan dan pesisir, untuk membangun citra program studi di tingkat Indonesia Timur.
+                        Mengembangkan potensi mahasiswa di bidang Teknologi Informasi dan Komunikasi wilayah pedesaan
+                        dan pesisir, untuk membangun citra program studi di tingkat Indonesia Timur.
                     </li>
                     <li>
                         Mengembangkan kampus hijau untuk mendukung penyelenggaraan tridharma perguruan tinggi.
