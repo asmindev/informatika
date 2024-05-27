722983fcd9899750f10cb82675f875d12a3be79b
iqkyu5566 <ilham5566@gmail.com>
2024-05-27 18:31:13 +0800

    feat: beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -3,31 +3,40 @@
         <div class="w-full uppercase">
             <h3 class="text-xl md:text-3xl font-medium md:font-semibold text-slate-800">Hi, Selamat datang di</h3>
             <h1 class="text-3xl md:text-6xl font-bold md:font-extrabold text-slate-800">
-                Jurusan Teknik Informatika Tes
+                Jurusan Teknik Informatika
             </h1>
-            <h2 class="text-xl font-medium md:text-3xl md:font-bold text-slate-800">Fakultas Teknik, Universitas Haluoleo, Kendari</h2>
+            <h2 class="text-xl font-medium md:text-3xl md:font-bold text-slate-800">Fakultas Teknik, Universitas
+                Haluoleo, Kendari</h2>
         </div>
     </section>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L21.8,170.7C43.6,181,87,203,131,202.7C174.5,203,218,181,262,186.7C305.5,192,349,224,393,218.7C436.4,213,480,171,524,176C567.3,181,611,235,655,234.7C698.2,235,742,181,785,176C829.1,171,873,213,916,229.3C960,245,1004,235,1047,197.3C1090.9,160,1135,96,1178,90.7C1221.8,85,1265,139,1309,186.7C1352.7,235,1396,277,1418,298.7L1440,320L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0099ff" fill-opacity="1"
+            d="M0,160L21.8,170.7C43.6,181,87,203,131,202.7C174.5,203,218,181,262,186.7C305.5,192,349,224,393,218.7C436.4,213,480,171,524,176C567.3,181,611,235,655,234.7C698.2,235,742,181,785,176C829.1,171,873,213,916,229.3C960,245,1004,235,1047,197.3C1090.9,160,1135,96,1178,90.7C1221.8,85,1265,139,1309,186.7C1352.7,235,1396,277,1418,298.7L1440,320L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z">
+        </path>
+    </svg>
     <section class="bg-blue-100">
         <div class="px-4 w-full md:w-11/12 lg:w-10/12 mx-auto py-12 min-h-[50vh] md:min-h-[80vh] flex items-center">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-center">
                 <div class="bg-white p-6 rounded-xl">
-                    <img class="rounded w-36 h-36" src="https://i.pravatar.cc/150?u={{ rand() }}" alt="Extra large avatar">
+                    <img class="rounded w-36 h-36" src="https://i.pravatar.cc/150?u={{ rand() }}"
+                        alt="Extra large avatar">
                 </div>
                 <div class="bg-white p-6 rounded-xl">
                     <a href="https://www.haloomedia.com/">
-                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg" alt="Extra large avatar">
+                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg"
+                            alt="Extra large avatar">
                     </a>
                 </div>
                 <div class="bg-white p-6 rounded-xl">
                     <a href="https://www.haloomedia.com/">
-                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg" alt="Extra large avatar">
+                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg"
+                            alt="Extra large avatar">
                     </a>
                 </div>
                 <div class="bg-white p-6 rounded-xl">
                     <a href="https://www.haloomedia.com/">
-                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg" alt="Extra large avatar">
+                        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg"
+                            alt="Extra large avatar">
                     </a>
                 </div>
             </div>
@@ -40,8 +49,10 @@
 
         <div class="px-4 w-full md:w-11/12 lg:w-10/12 mx-auto py-12 min-h-[50vh] md:min-h-[80vh] flex items-center">
             <div class="w-full flex justify-between items-center flex-col md:flex-row">
-                <div class="w-full md:w-1/2 relative rounded-full overflow-hidden after:absolute after:bg-indigo-500 after:bottom-0 after:w-full after:h-12 after:left-1/2  before:absolute before:bg-orange-600 before:bottom-12 before:w-full before:h-24 before:md:h-32">
-                    <img class="w-full h-full object-cover scale-150" src="{{ asset('storage/file.png') }}" alt="banner">
+                <div
+                    class="w-full md:w-1/2 relative rounded-full overflow-hidden after:absolute after:bg-indigo-500 after:bottom-0 after:w-full after:h-12 after:left-1/2  before:absolute before:bg-orange-600 before:bottom-12 before:w-full before:h-24 before:md:h-32">
+                    <img class="w-full h-full object-cover scale-150" src="{{ asset('storage/file.png') }}"
+                        alt="banner">
                     <div class="z-50 absolute bg-black/50 backdrop-blur-md w-full h-24 md:h-40  bottom-0 left-0">
                         <div class="w-full h-full flex justify-center items-center flex-col">
                             <h1 class="text-lg md:text-3xl font-bold text-white">Isnawaty, S.Si., M.T</h1>
@@ -57,10 +68,19 @@
                     </div>
                     <article class=" text-slate-700 mt-8 flex flex-col gap-y-6">
                         <p>
-                            Program Studi S1 Teknik Informatika Universitas Halu Oleo adalah salah satu jurusan yang berada di Universitas Halu Oleo. Program Studi Teknik Informatika UHO merupakan bagian dari Fakultas Teknik Universitas Halu Oleo sejak dari tahun 2007 serta hingga saat ini didukung oleh 19 tenaga pengajar dengan kualifikasi lulusan dari universitas – universitas ternama yang berada di Indonesia.
+                            Program Studi S1 Teknik Informatika Universitas Halu Oleo adalah salah satu jurusan yang
+                            berada di Universitas Halu Oleo. Program Studi Teknik Informatika UHO merupakan bagian dari
+                            Fakultas Teknik Universitas Halu Oleo sejak dari tahun 2007 serta hingga saat ini didukung
+                            oleh 19 tenaga pengajar dengan kualifikasi lulusan dari universitas – universitas ternama
+                            yang berada di Indonesia.
                         </p>
                         <p>
-                            Selain itu, telah banyak memiliki prestasi yang diraih oleh mahasiswa IT UHO baik tingkat daerah maupun tingkat nasional seperti GEMASTIK, Socio Technopreneur Campus (STC), Kompetisi Debat Mahasiswa Indonesia (KDMI), serta berbagai prestasi lainnya baik di bidang akademik maupun non akademik. Hingga saat ini, Teknik Informatika UHO memiliki tiga bidang minat studi yakni Rekayasa Perangkat Lunak (RPL), Komputasi Cerdas dan Visualisasi (KCV), serta Komputasi Berbasis Jaringan (KBJ).
+                            Selain itu, telah banyak memiliki prestasi yang diraih oleh mahasiswa IT UHO baik tingkat
+                            daerah maupun tingkat nasional seperti GEMASTIK, Socio Technopreneur Campus (STC), Kompetisi
+                            Debat Mahasiswa Indonesia (KDMI), serta berbagai prestasi lainnya baik di bidang akademik
+                            maupun non akademik. Hingga saat ini, Teknik Informatika UHO memiliki tiga bidang minat
+                            studi yakni Rekayasa Perangkat Lunak (RPL), Komputasi Cerdas dan Visualisasi (KCV), serta
+                            Komputasi Berbasis Jaringan (KBJ).
                         </p>
                     </article>
                 </div>
@@ -75,18 +95,19 @@
         </div>
         <div class="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 auto-rows-fr">
             @foreach ($news as $n)
-            <a href="/berita/{{ $n->id }}" class="w-full h-fit rounded-xl overflow-hidden border border-slate-200">
-                <img src="{{ $n->image }}" alt="{{ $n->title }}" class="w-full h-30 object-cover">
-                <div class="w-full h-full">
-                    <div class="py-6 px-4">
-                        <div class="w-full flex justify-between">
-                            <h1 class="text-slate-800 font-medium text-lg">{{ $n->title }}</h1>
-                            <h2 class="text-slate-600 text-sm">{{ $n->date }}</h2>
+                <a href="/berita/{{ $n->id }}"
+                    class="w-full h-fit rounded-xl overflow-hidden border border-slate-200">
+                    <img src="{{ $n->image }}" alt="{{ $n->title }}" class="w-full h-30 object-cover">
+                    <div class="w-full h-full">
+                        <div class="py-6 px-4">
+                            <div class="w-full flex justify-between">
+                                <h1 class="text-slate-800 font-medium text-lg">{{ $n->title }}</h1>
+                                <h2 class="text-slate-600 text-sm">{{ $n->date }}</h2>
+                            </div>
+                            <h3 class="text-slate-600 text-sm line-clamp-2">{{ $n->content }}</h3>
                         </div>
-                        <h3 class="text-slate-600 text-sm line-clamp-2">{{ $n->content }}</h3>
                     </div>
-                </div>
-            </a>
+                </a>
             @endforeach
         </div>
     </section>
